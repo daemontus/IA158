7fc751ed6a0e6a46e79e331f9802651c3250357e
Samuel Pastva <daemontus@gmail.com>
2017-05-04 18:35:58 +0200

    latest code

--- src/main/java/ia158/Main.java
+++ src/main/java/ia158/Main.java
@@ -15,8 +15,10 @@ import java.net.InetAddress;
  */
 public class Main {
 
-    private static long START_SEARCHING_TIME = 3000;
-    private static long TIME_BEFORE_SHOOT = 1000;
+    private static final long START_SEARCHING_TIME = 3000;
+    private static final long TIME_BEFORE_SHOOT = 1000;
+    // Speed: fastest 50, the bigger the slower
+    private static final int SPEED = 90;
 
     private static String eduroam = "147.251.45.255";
     private static String robot = "10.0.1.255";
@@ -39,9 +41,12 @@ public class Main {
         RegulatedMotor shoot = new EV3MediumRegulatedMotor(MotorPort.C);
 
         float MAX_SPEED = rightWheels.getMaxSpeed();
+        rightWheels.setSpeed(Math.round(rightWheels.getMaxSpeed()) / 2);
+
+        System.out.println("MAX SPEED" + MAX_SPEED);
 
         // Run
-        while (System.currentTimeMillis() < start + 60000) {
+        while (System.currentTimeMillis() < start + 20000) {
             // receive packet
             byte[] buffer = new byte[1];
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
@@ -53,8 +58,10 @@ public class Main {
             if (action.isRight()) {
                 lostTargetTime = null;
                 targetingTime = null;
-                rightWheels.setSpeed(Math.round(((action.getValue() - 50) / 50) * MAX_SPEED));
-                leftWheels.setSpeed(Math.round(((action.getValue() - 50) / 50) * MAX_SPEED));
+                rightWheels.setSpeed(Math.round(((float) (action.getValue() - 50) / SPEED) * MAX_SPEED));
+                leftWheels.setSpeed(Math.round(((float) (action.getValue() - 50) / SPEED) * MAX_SPEED));
+                System.out.println(Math.round(((float) (action.getValue() - 50) / SPEED) * MAX_SPEED));
+                System.out.println(Math.round(((float) (action.getValue() - 50) / SPEED) * MAX_SPEED));
                 if (!lastAction.isRight()) {
                     // start rotating right
                     rightWheels.backward();
@@ -66,8 +73,12 @@ public class Main {
             if (action.isLeft()) {
                 lostTargetTime = null;
                 targetingTime = null;
-                rightWheels.setSpeed(Math.round(((action.getValue() + 5)/ 45) * MAX_SPEED));
-                leftWheels.setSpeed(Math.round(((action.getValue() + 5)/ 45) * MAX_SPEED));
+                rightWheels.setSpeed(Math.round(rightWheels.getMaxSpeed()) / 2);
+                rightWheels.setSpeed(Math.round(((float) (50 - action.getValue())/ SPEED) * MAX_SPEED));
+                leftWheels.setSpeed(Math.round(((float) (50 - action.getValue())/ SPEED) * MAX_SPEED));
+                leftWheels.
+                System.out.println(Math.round(((float) (50 - action.getValue())/ SPEED) * MAX_SPEED));
+                System.out.println(Math.round(((float) (50 - action.getValue())/ SPEED) * MAX_SPEED));
                 if (!lastAction.isLeft()) {
                     // start rotating left
                     rightWheels.forward();
