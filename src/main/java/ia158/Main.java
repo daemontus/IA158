035fbf88371579251752878b46d823550cf81593
Jiří Mauritz <jirmauritz@gmail.com>
2017-05-23 11:53:34 +0200

    Testing new structure of robot

--- src/main/java/ia158/Main.java
+++ src/main/java/ia158/Main.java
@@ -20,37 +20,68 @@ public class Main {
     // Speed: fastest 50, the bigger the slower
     private static final int SPEED = 90;
 
+    private static RegulatedMotor direction;
+    private static RegulatedMotor shoot;
+    private static RegulatedMotor aim;
+
     private static String eduroam = "147.251.45.255";
     private static String robot = "10.0.1.255";
     private static Long lostTargetTime;
     private static Long targetingTime;
     private static Action lastAction = new Action(-1);
 
+    // TESTING
+    private static byte get_value(long starttime) {
+        int step = 3000;
+        if (System.currentTimeMillis() - starttime < step) {
+            return (byte) 0;
+        }
+        if (System.currentTimeMillis() - starttime < 2*step) {
+            return (byte) 255;
+        }
+        if (System.currentTimeMillis() - starttime < 3*step) {
+            return (byte) 0;
+        }
+        if (System.currentTimeMillis() - starttime < 4*step) {
+            return (byte) 30;
+        }
+        if (System.currentTimeMillis() - starttime < 5*step) {
+            return (byte) 40;
+        }
+        return 50;
+    }
+    // TESTING
+
     public static void main(String[] args) throws IOException {
 
         // Init
-        InetAddress group = InetAddress.getByName(robot);
-        DatagramSocket socket = new DatagramSocket(9999, group);
+        //InetAddress group = InetAddress.getByName(robot);
+        //DatagramSocket socket = new DatagramSocket(9999, group);
         long start = System.currentTimeMillis();
         lostTargetTime = start;
         targetingTime = null;
 
         // Motors
-        RegulatedMotor rightWheels = new EV3LargeRegulatedMotor(MotorPort.A);
-        RegulatedMotor leftWheels = new EV3LargeRegulatedMotor(MotorPort.B);
-        RegulatedMotor shoot = new EV3MediumRegulatedMotor(MotorPort.C);
+        direction = new EV3LargeRegulatedMotor(MotorPort.B);
+        shoot = new EV3LargeRegulatedMotor(MotorPort.D);
+        aim = new EV3MediumRegulatedMotor(MotorPort.C);
 
-        float MAX_SPEED = rightWheels.getMaxSpeed();
-        rightWheels.setSpeed(Math.round(rightWheels.getMaxSpeed()) / 2);
+        float MAX_SPEED = direction.getMaxSpeed() / 4;
+        direction.setSpeed(Math.round(MAX_SPEED) / 2);
 
         System.out.println("MAX SPEED" + MAX_SPEED);
 
         // Run
-        while (System.currentTimeMillis() < start + 20000) {
+        while (System.currentTimeMillis() < start + 18000) {
             // receive packet
             byte[] buffer = new byte[1];
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-            socket.receive(packet);
+            //DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            //socket.receive(packet);
+
+            // TESTING
+            buffer[0] = get_value(start);
+            System.out.println("value: " + (int) buffer[0]);
+            // TESTING
 
             Action action = Action.fromByte(buffer[0]);
             System.out.println("Action: "+action);
@@ -58,14 +89,11 @@ public class Main {
             if (action.isRight()) {
                 lostTargetTime = null;
                 targetingTime = null;
-                rightWheels.setSpeed(Math.round(((float) (action.getValue() - 50) / SPEED) * MAX_SPEED));
-                leftWheels.setSpeed(Math.round(((float) (action.getValue() - 50) / SPEED) * MAX_SPEED));
-                System.out.println(Math.round(((float) (action.getValue() - 50) / SPEED) * MAX_SPEED));
+                direction.setSpeed(Math.round(((float) (action.getValue() - 50) / SPEED) * MAX_SPEED));
                 System.out.println(Math.round(((float) (action.getValue() - 50) / SPEED) * MAX_SPEED));
                 if (!lastAction.isRight()) {
                     // start rotating right
-                    rightWheels.backward();
-                    leftWheels.forward();
+                    direction.backward();
                     lastAction = action;
                 }
             }
@@ -73,23 +101,18 @@ public class Main {
             if (action.isLeft()) {
                 lostTargetTime = null;
                 targetingTime = null;
-                rightWheels.setSpeed(Math.round(rightWheels.getMaxSpeed()) / 2);
-                rightWheels.setSpeed(Math.round(((float) (50 - action.getValue())/ SPEED) * MAX_SPEED));
-                leftWheels.setSpeed(Math.round(((float) (50 - action.getValue())/ SPEED) * MAX_SPEED));
-                System.out.println(Math.round(((float) (50 - action.getValue())/ SPEED) * MAX_SPEED));
+                direction.setSpeed(Math.round(((float) (50 - action.getValue())/ SPEED) * MAX_SPEED));
                 System.out.println(Math.round(((float) (50 - action.getValue())/ SPEED) * MAX_SPEED));
                 if (!lastAction.isLeft()) {
                     // start rotating left
-                    rightWheels.forward();
-                    leftWheels.backward();
+                    direction.forward();
                     lastAction = action;
                 }
             }
 
             if (action.isShoot()) {
                 // stop rotating
-                rightWheels.stop(true);
-                leftWheels.stop(true);
+                direction.stop(true);
 
                 lostTargetTime = null;
                 if (targetingTime == null) {
@@ -106,8 +129,7 @@ public class Main {
                 if (lastAction.isShoot()) {
                     if (lostTargetTime != null && System.currentTimeMillis() > lostTargetTime + START_SEARCHING_TIME) {
                         // start searching
-                        rightWheels.backward();
-                        leftWheels.forward();
+                        direction.forward();
                         lastAction = new Action(90);
                         lostTargetTime = null;
                     } else if (lostTargetTime == null) {
